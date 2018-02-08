1c393450398c5c6b5556a4d54d7856c7f3a88719
yanghanlin200 <35319321+yanghanlin200@users.noreply.github.com>
2018-02-08 11:52:42 +0800

    测试

---
+++ App/Admin/Controller/IndexController.class.php
@@ -0,0 +1,17 @@
+<?php
+
+class IndexController extends CommonController
+{
+    //显示首页的模板
+    public function index()
+    {
+
+//      echo 'hellow world';
+        $this->display('_index.html');
+
+
+    }
+
+}
+
+?>
